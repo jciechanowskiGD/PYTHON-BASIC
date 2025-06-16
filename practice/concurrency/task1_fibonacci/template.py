43b3be0cb30c5fcf55be2ba41615c3dbfcc62ebb
Jedrzej Ciechanowski <jciechanowski@griddynamics.com>
2025-06-16 23:30:59 +0200

    concurrency: completed

--- practice/concurrency/task1_fibonacci/template.py
+++ practice/concurrency/task1_fibonacci/template.py
@@ -1,31 +1,58 @@
 import os
+import sys
 from random import randint
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+import time
 
-
-OUTPUT_DIR = './output'
-RESULT_FILE = './output/result.csv'
+OUTPUT_DIR = "./output"
+RESULT_FILE = "./output/result.csv"
+sys.set_int_max_str_digits(100000)  # increasing limit bcs large numbers
 
 
 def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
 
     f0, f1 = 0, 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         f0, f1 = f1, f0 + f1
     return f1
 
 
 def func1(array: list):
-    pass
+    with ProcessPoolExecutor() as executor:
+        for num, fib_res in zip(array, executor.map(fib, array)):
+            with open(f"{OUTPUT_DIR}/temp/{num}.txt", "w") as file:
+                file.write(str(fib_res))
 
 
-def func2(result_file: str):
-    pass
+def open_files(filepath: str):
+    with open(f"./{OUTPUT_DIR}/temp/{filepath}", "r") as f:
+        return f.read()
 
 
-if __name__ == '__main__':
+def func2(output_dir: str):
+    files = os.listdir(f"./{OUTPUT_DIR}/temp")
+    nums = []
+    with ThreadPoolExecutor() as executor:
+        future_file = {executor.submit(open_files, file): file[:-4] for file in files}
+        for future in as_completed(future_file):
+            nums.append(
+                (
+                    future_file[future],
+                    future.result(),
+                )
+            )
+
+    with open(RESULT_FILE, "w") as f:
+        for pair in nums:
+            f.write(f"{pair[0]},{pair[1]}\n")
+
+
+if __name__ == "__main__":
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
+    if not os.path.exists(OUTPUT_DIR + "/temp"):
+        os.makedirs(OUTPUT_DIR + "/temp")
 
     func1(array=[randint(1000, 100000) for _ in range(1000)])
-    func2(result_file=RESULT_FILE)
+    func2(output_dir=OUTPUT_DIR)
--- practice/concurrency/task2_apod_api/template.py
+++ practice/concurrency/task2_apod_api/template.py
@@ -1,24 +1,45 @@
-API_KEY = "YOUR_KEY"
-APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
-OUTPUT_IMAGES = './output'
+from api_key import key
+import requests
+import json
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+API_KEY = key()
+APOD_ENDPOINT = "https://api.nasa.gov/planetary/apod"
+OUTPUT_IMAGES = "./output_images"
 
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    response = requests.get(
+        f"{APOD_ENDPOINT}?api_key={api_key}&start_date={start_date}&end_date={end_date}"
+    )
+    return json.loads(response.content)
+
+
+def download_image(day: dict):
+    img = requests.get(day["url"])
+    with open(f"{OUTPUT_IMAGES}/{day['date']}.jpg", "wb") as f:
+        f.write(img.content)
 
 
 def download_apod_images(metadata: list):
-    pass
+    with ThreadPoolExecutor() as executor:
+        executor.map(
+            download_image, (day for day in metadata if day["media_type"] == "image")
+        )
 
 
 def main():
     metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
+        start_date="2021-08-01",
+        end_date="2021-09-30",
         api_key=API_KEY,
     )
+    print(metadata)
     download_apod_images(metadata=metadata)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.makedirs(OUTPUT_IMAGES)
     main()
